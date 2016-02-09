2c30da1233b7c63ae0769c48c45aa2b6f0a4e907
Tim W <tim.weiss2013@gmail.com>
2016-02-09 21:09:29 +0100

    Animes in Tabelle darstellen
    Die Animes aus der Datenbank werden jetzt in einer Tabelle dargestellt,
    damit es möglichst responsive bleibt.

--- resources/views/anime/index.blade.php
+++ resources/views/anime/index.blade.php
@@ -8,23 +8,29 @@
 
 {{-- Seiteninhalt --}}
 @section('content')
-    <div class="row">
-        <div class="col-lg-12">
-            <h2 class="page-header">Anime</h2>
-        </div>
-            @foreach($animes as $anime)
-                <div class="col-md-4 col-sm-4">
-                    <a href="{{ url('anime', [$anime->id]) }}">
-                        <div class="anime-item">
-                            <img class="anime-cover" src="" />
-                            <p class="anime-name">{{ $anime->name }}</p>
-                            <p class="anime-description">{!! $anime->description !!}</p>
-                            <p class="anime-meta">{{ $anime->episodes }} Folgen &#64; {{ $anime->episodes_duration }} Minuten</p>
+        <div class="row">
+            <div class="page-container">
+                <div class="col-md-10">
+                    <div class="page-content-header">
+                        <h2 class="page-header">Anime</h2>
+                    </div>
+                        <div class="panel panel-default table-container">
+                            <table class="table-bordered table-anime">
+                                <tr class="table-head"><th class="table-head-item" id="table-cover">Cover</th><th class="table-head-item" id="table-name">Name</th><th class="table-head-item" id="table-episodes">Folgen</th></tr>
+                                <tbody>
+                                @foreach($animes as $anime)
+                                        <tr class="anime-table-row">
+                                            <td class="table-cover-image"><img class="table-cover-item" src="https://storage.googleapis.com/dissary/dawn/images/covers/{{ $anime->imageurl }}" /></td>
+                                            <td class="table-item table-name-item"><a href="/anime/{{ $anime->id }}">{{ $anime->name }}</a></td>
+                                            <td class="table-item table-episodes-item">{{ $anime->episodes }}</td>
+                                        </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
                         </div>
-                    </a>
                 </div>
-            @endforeach
-    </div>
+            </div>
+        </div>
 @stop
 
 {{-- Skripte --}}
@@ -35,5 +41,76 @@
 {{-- CSS Dateien --}}
 @section('css')
     {!! Html::style('css/anime-list.css') !!}
-    <style> #page-wrapper { margin-top:30px; }</style>
+    <style>
+        #page-wrapper {
+            margin-top:30px;
+        }
+        .anime-item{
+            /*
+            height:150px;
+            width:auto;
+            background-color:#FFFFFF;
+            border-radius:5px;
+            border:#cccccc 1px solid;
+            margin-top:10px;
+            */
+        }
+        .anime-table-row{
+            height:75px;
+        }
+        .table-anime{
+            width:100%;
+            border-radius: 2.5px;
+        }
+        .page-container{
+            padding:10px;
+            background-color:#ffffff;
+        }
+        .page-content-header{
+            background-color:#ffffff;
+            margin-bottom:10px;
+        }
+        .table-item{
+            padding:10px;
+        }
+        .table-head-item{
+            padding-left:10px;
+            padding-right:10px;
+            border-top:none !important;
+        }
+        .table-name-item{
+            font-size:22px;
+            font-family:"Exo 2", sans-serif;
+        }
+        .table-head{
+            height:34px;
+            background: linear-gradient(to bottom, #ffffff 0%,#EFEFEF 100%);
+        }
+        .table-cover-item{
+            position:relative;
+            height:100%;
+        }
+        .table-cover-image{
+           height:75px;
+           text-align: center;
+        }
+        .table-container{
+            box-shadow: 0 1px 3px rgba(0,0,0,.15);
+        }
+        .table-episodes-item{
+            font-size:18px;
+            font-family:"Exo 2", sans-serif;
+        }
+
+        @media (max-width: 512px){
+            .table-name-item{
+                font-size:14px;
+                font-family:"Exo 2", sans-serif;
+            }
+            .table-episodes-item{
+                font-size:14px;
+                font-family:"Exo 2", sans-serif;
+            }
+        }
+    </style>
 @stop
